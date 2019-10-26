b334fee8698fa86b2e3fa09062d030afefd284c7
huhisoft <huhibrowser@gmail.com>
2019-10-26 09:53:03 +0700

    add test

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # autoplay-whitelist
+
+C++ autoplay whitelist parser for Huhi
+
+## Setup
+
+```
+npm install --save autoplay-whitelist
+```
+
+## Installation
+
+1. Clone the git repository from GitHub:
+
+        git clone https://github.com/huhisoft/autoplay-whitelist
+
+2. Open the working directory:
+
+        cd autoplay-whitelist
+
+3. Install the Node (v5+) dependencies:
+
+        npm install
+
+## Build the node addon
+
+```
+npm run install
+```
+
+## Generate the DAT file
+
+```
+npm run data-files
+```
+
+## Running tests
+
+```
+npm run test
+```
---
+++ autoplay_whitelist_parser.h
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef AUTOPLAY_WHITELIST_PARSER_H_
+#define AUTOPLAY_WHITELIST_PARSER_H_
+
+#include <memory>
+
+#include "autoplay_whitelist_data.h"
+
+#define AUTOPLAY_DAT_FILE "AutoplayWhitelist.dat"
+#define AUTOPLAY_DAT_FILE_VERSION "1"
+
+template<class T>
+class HashSet;
+
+class AutoplayWhitelistParser {
+public:
+  AutoplayWhitelistParser();
+  ~AutoplayWhitelistParser();
+
+  void addHost(const char *inputHost);
+  bool matchesHost(const char *inputHost);
+
+  // Serializes the parsed data into a single buffer.
+  // The returned buffer should be deleted.
+  char* serialize(unsigned int* totalSize);
+
+  // Deserializes the buffer, a size is not needed since a serialized
+  // buffer is self described
+  bool deserialize(char *buffer);
+  bool deserialize(char *buffer, size_t);
+
+private:
+  std::unique_ptr<HashSet<ST_AUTOPLAY_WHITELIST_DATA> > mHosts;
+};
+
+#endif  // AUTOPLAY_WHITELIST_PARSER_H_
